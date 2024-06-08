e14b0c91d13d7abc2925728c4a8d5412ee27efa0
Bongjung Kim <apple75391@gmail.com>
2024-06-08 22:12:11 +0900

    장바구니 수량 변동시 가격변동 기능 수정

--- js/basket.js
+++ js/basket.js
@@ -3,47 +3,58 @@ $(document).ready(function(){
     /////////////////////////////////////////////////////////
     /////////// 제품 수량 변동 시 가격 변동 시작///////////////
     /////////////////////////////////////////////////////////
+    // $(".price").text("₩ " + pricePerItem.toLocaleString() + "원");
 
-    ///////// 제품 수량 변동 시작 ///////
-    // $(".plus").click(function(){
-    //     var $input = $(this).prev(".qty");
-    //     var newValue = parseInt($input.val()) + 1;
-    //     $input.val(newValue);
-    // });
-    
-    // $(".minus").click(function(){
-    //     var $input = $(this).next(".qty");
-    //     var newValue = parseInt($input.val()) - 1;
-    //     if(newValue >= 1) {
-    //         $input.val(newValue);
-    //     }
-    // });
-    ///////// 제품 수량 변동 끝 ///////
-
-    // 초기 가격 설정
-    var pricePerItem = 60000;
-    $(".price").text("₩ " + pricePerItem.toLocaleString() + "원");
-
+    // + 클릭 시 실행
     $(".plus").click(function(){
+        // (+) 클릭하면 현재 수량 확인하고 +1 해서 다시 input값에 넣어주는거임
         var $input = $(this).prev(".qty");
         var newValue = parseInt($input.val()) + 1;
         $input.val(newValue);
+        
+        // hidden에 숨겨둔 제품 가격을 가져와서 숫자로만 만들어주는거
+        var $price = $(this).closest('.items_count_body').next('.items_price_body').find('.price_input_hidden');
+        var $price = $price.val().replace(",", "").replace("원","");
+        
+        // console.log($price); //테스트용 콘솔로그
 
-        updatePrice(newValue);
+        // 수량이랑 가격 넣어서 함수 호출 계산해서 반환값 리턴함
+        var $price = updatePrice(newValue, $price);
+        // 숫자를 "," 넣어서 200,000 식으로 포맷
+        var $price = new Intl.NumberFormat().format($price);
+        console.log($price);
+
+        $(this).closest('.items_count_body').next('.items_price_body').find('.price_input').val("₩ " +$price+"원");
     });
-
+        
+    // - 클릭 시 실행
     $(".minus").click(function(){
         var $input = $(this).next(".qty");
         var newValue = parseInt($input.val()) - 1;
         if(newValue >= 1) {
             $input.val(newValue);
-            updatePrice(newValue);
+
+            // hidden에 숨겨둔 제품 가격을 가져와서 숫자로만 만들어주는거
+            var $price = $(this).closest('.items_count_body').next('.items_price_body').find('.price_input_hidden');
+            var $price = $price.val().replace(",", "").replace("원","");
+            
+            // console.log($price); //테스트용 콘솔로그
+    
+            // 수량이랑 가격 넣어서 함수 호출 계산해서 반환값 리턴함
+            var $price = updatePrice(newValue, $price);
+            // 숫자를 "," 넣어서 200,000 식으로 포맷
+            var $price = new Intl.NumberFormat().format($price);
+            console.log($price);
+            $(this).closest('.items_count_body').next('.items_price_body').find('.price_input').val("₩ " +$price+"원");
         }
     });
 
-    function updatePrice(quantity) {
-        var totalPrice = pricePerItem * quantity;
-        $(".price").text("₩ " + totalPrice.toLocaleString() + "원");
+    function updatePrice(quantity, price) {
+        //테스트용
+        // console.log(quantity);
+        // console.log(price);
+
+        return quantity * price;
     }
 
     // + - 버튼 hover 시작 //
