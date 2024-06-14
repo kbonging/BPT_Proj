ccfba356415b07eabc75904c267746d282a82669
bongjung kim <apple75391@gmail.com>
2024-06-14 15:03:00 +0900

    글 상세보기 구현중

---
+++ js/notice.js
@@ -0,0 +1,9 @@
+$(function () {
+    // alert('test');
+
+    $('.board td:nth-child(2)').click(function(){
+        alert('구현중입니다.');
+        // location.href="boardDetail.html";
+    });
+
+}); // $(function () 끝
