b2f6331b27cfda52aa5614a3bb712c184315ff3f
Bongjung Kim <apple75391@gmail.com>
2024-06-27 20:35:08 +0900

    마이페이지 로그아웃 버튼 추가

--- js/aside.js
+++ js/aside.js
@@ -6,4 +6,22 @@ $(function () {
     $('.FAQ').click(function(){
         location.href="FAQ.html";
     });
+
+    //로그아웃(signOUt) 클릭 시 로그아웃
+    $('.signOUt').click(function(){
+        alert('로그아웃');
+        location.href="index.html";
+    });
+
+    //로그아웃 hover 시
+    $('.signOUt').hover(function(){
+        $(this).css({
+            'cursor': 'pointer',
+            'text-decoration': 'underline'
+        });
+    }, function(){
+        $(this).css({
+            'text-decoration': 'none'
+        });
+    });
 });
