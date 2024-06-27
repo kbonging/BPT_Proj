7aed8a98b36218d1281412443f38c39911c9a24b
Bongjung Kim <apple75391@gmail.com>
2024-06-27 22:37:52 +0900

    글 쓰기 페이지 구현 글 상세페이지 구현

--- js/notice.js
+++ js/notice.js
@@ -2,8 +2,21 @@ $(function () {
     // alert('test');
 
     $('.board td:nth-child(2)').click(function(){
-        alert('구현중입니다.');
-        // location.href="boardDetail.html";
+        // alert('업데이트중입니다.');
+        location.href="boardDetail.html";
+    });
+
+    // 글쓰기버튼 호버
+    $(".boardWriteBtn").hover(function(){
+        $(this).css("background-color", "#ccc");
+    }, function(){
+        $(this).css("background-color", "");
+    });
+
+    // 글쓰기버튼 클릭 시
+    $(".boardWriteBtn").click(function(){
+        // alert('test');
+        location.href="boardWrite.html";
     });
 
 }); // $(function () 끝
